9857feeacd64db690ed1c949a9f12222b0b789f0
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-06 14:17:10 -0300

    add colors

--- ex017 - Collared and Hypotenuse .py
+++ ex017 - Collared and Hypotenuse .py
@@ -8,7 +8,6 @@ colors = {"clean": "\033[m",
           "cian": "\033[36m"}
 ca = float(input("Type the value of the adjacent over: "))
 co = float(input("Type the value of the opposite over: "))
-'''hyp = (pow(ca, 2) + pow(co, 2)) ** (1/2)'''
 hyp = hypot(co, ca)
 print(" The adjacent over is {}{:.2f}{} and the opposite over is {}{:.2f}{} "
       "\n Doing the count we have {}{:.2f}{} like Hypotenuse"
