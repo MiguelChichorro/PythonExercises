70556af10a2b33448d0b555e684d4d8ce347f80d
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-06 14:08:19 -0300

    add colors

--- ex016 - Function Trunc.py
+++ ex016 - Function Trunc.py
@@ -1,3 +1,4 @@
+from math import trunc
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -5,6 +6,11 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
-from math import trunc
 n1 = float(input("Type a real number: "))
-print("Your number is {:.2f} and using the function trunc is {} \nBut we can use the function int to {}".format(n1, trunc(n1), int(n1)))
+print("Your number is {}{:.2f}{} and using the function trunc is {}{}{}"
+      "\nBut we can use the function int to {}{}{}"
+      "\n{}the two doing the same thing{}"
+      .format(colors["cian"], n1, colors["clean"],
+              colors["cian"], trunc(n1), colors["clean"],
+              colors["cian"], int(n1), colors["clean"],
+              colors["green"], colors["clean"]))
