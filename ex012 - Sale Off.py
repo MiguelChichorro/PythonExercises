b9ef6bab138a1d4ea2daac9a3598122951bd0c81
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-06 10:52:37 -0300

    add colors

--- ex012 - Sale Off.py
+++ ex012 - Sale Off.py
@@ -7,4 +7,5 @@ colors = {"clean": "\033[m",
           "cian": "\033[36m"}
 n1 = float(input("Type the product price: R$"))
 sale = n1*0.05
-print("The price with 5% off is R${:.2f}".format(n1 - sale))
+print("The price with 5% off is {}R${:.2f}{}"
+      .format(colors["green"], n1 - sale, colors["clean"]))
