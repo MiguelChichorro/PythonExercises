d17d5d10274fb728baaea9ef7dbdfbf15dd3b005
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-06 10:43:43 -0300

    colors

--- ex011 - Height, Width and Area.py
+++ ex011 - Height, Width and Area.py
@@ -9,4 +9,7 @@ n1 = float(input("Type in meters the height of your wall: "))
 n2 = float(input("Type in meters the the width of your wall: "))
 area = n1 * n2
 paint = area / 2
-print("Your wall are is {}m² and you need {} liters of paint for paint all the wall".format(area, paint))
+print("Your wall are is {}{}{}m²"
+      "\nand you need {}{}{} liters of paint for paint all the wall"
+      .format(colors["blue"], area, colors["clean"],
+              colors["yellow"], paint, colors["clean"]))
