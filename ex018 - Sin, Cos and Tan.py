275de349447234cfaae34dffc1abfd749f407a95
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-06 14:19:37 -0300

    add colors

--- ex018 - Sin, Cos and Tan.py
+++ ex018 - Sin, Cos and Tan.py
@@ -1,3 +1,4 @@
+from math import sin, cos,  tan, radians
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -5,12 +6,15 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
-from math import sin, cos,  tan, radians
 n1 = float(input("Type a number :"))
 Sin = sin(radians(n1))
 Cos = cos(radians(n1))
 Tan = tan(radians(n1))
-print("Your number is {} "
-      "\n Your SIN is {:.2f} "
-      "\n Your COS {:.2f} "
-      "\n The last is TAN the yours is {:.2f}".format(n1, Sin, Cos, Tan))
+print("{}Your number is {}{}"
+      "\n{}Your SIN is {:.2f}{}"
+      "\n{}Your COS {:.2f}{}"
+      "\n{}The last is TAN the yours is {:.2f}{}"
+      .format(colors["cian"], n1, colors["clean"],
+              colors["purple"], Sin, colors["clean"],
+              colors["cian"], Cos, colors["clean"],
+              colors["purple"], Tan, colors["clean"]))
