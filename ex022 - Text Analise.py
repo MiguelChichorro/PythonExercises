e89294102b5bdb8d4016174af450949c6fc29cc0
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-07 11:00:12 -0300

    add colors, function sleep and two if

--- ex022 - Text Analise.py
+++ ex022 - Text Analise.py
@@ -1,3 +1,4 @@
+from time import sleep
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -6,8 +7,20 @@ colors = {"clean": "\033[m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
 name = str(input("Hello, please tell me your name: ")).strip()
-print("your name with all letter up is {}"
-      "\n and with all letters lower is {}".format(name.upper(), name.lower()))
+print("{}Hmm...let me see{}".format(colors["cian"], colors["clean"]))
+sleep(2)
+print("your name with all letter up is {}{}{}"
+      "\nAnd with all letters lower is {}{}{}"
+      .format(colors["cian"], name.upper(), colors["clean"],
+              colors["yellow"], name.lower(), colors["clean"]))
 division = name.split()
-print("Your name has {} letters and your first name is {} and has {} letters"
-      "\n Nice to meet you {}".format(len(name) - name.count(" "), division[0], len(division[0]), name))
+print("Your name has {}{}{} letters"
+      "\nYour first name is {}{}{} and has {}{}{} letters"
+      .format(colors["green"], len(name) - name.count(" "), colors["clean"],
+              colors["green"], division[0], colors["clean"],
+              colors["green"], len(division[0]), colors["clean"]))
+if len(name) - name.count(" ") > 25:
+    print("{}Omg your is very big{}".format(colors["red"], colors["clean"]))
+if division[0] == "Miguel":
+    print("{}Your name is very cool, congratulations!!!{}".format(colors["purple"], colors["clean"]))
+print("Nice to meet you {}{}{}".format(colors["blue"], name, colors["clean"]))
