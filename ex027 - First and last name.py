8a9384badb50f67f0d2166136c27d925015618f5
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-09 20:51:59 -0300

    add colors and function sleep

--- ex027 - First and last name.py
+++ ex027 - First and last name.py
@@ -1,3 +1,4 @@
+from time import sleep
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -6,7 +7,11 @@ colors = {"clean": "\033[m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
 name = str(input("Type your name please: ")).strip()
-print("Nice to meet you {}".format(name))
+print("{}Loading...{}".format(colors["green"], colors["clean"]))
+sleep(2)
+print("Nice to meet you {}{}{}".format(colors["blue"], name, colors["clean"]))
 name = name.split()
-print("First name {}"
-      "\nLast name {}".format(name[0], name[-1]))
+print("First name {}{}{}"
+      "\nLast name {}{}{}"
+      .format(colors["blue"], name[0], colors["clean"],
+              colors["blue"], name[-1], colors["clean"]))
