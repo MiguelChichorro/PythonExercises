2c34c2356e6ff6aa5b861cbf929baca8f2c0cc20
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-11 11:46:08 -0300

    Add colors and function sleep

--- ex035 - Analyze Triangle.py
+++ ex035 - Analyze Triangle.py
@@ -1,3 +1,4 @@
+from time import sleep
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -8,9 +9,11 @@ colors = {"clean": "\033[m",
 lin1 = float(input("Type the first length: "))
 lin2 = float(input("Type the second length: "))
 lin3 = float(input("Type the Third length: "))
+print("{}Loading...{}".format(colors["green"], colors["clean"]))
+sleep(2)
 if (lin1 + lin2) > lin3 and (lin1 + lin3) > lin2 and (lin2 + lin3) > lin1:
-    print("\033[32mYou can make a triangle with these lengths"
-          "\nBecause the sum between {} and {} is bigger than {}\033[m".format(lin1, lin2, lin3))
+    print("{}You can make a triangle with these lengths"
+          "\nBecause the sum between {} and {} is bigger than {}{}".format(colors["green"], lin1, lin2, lin3, colors["clean"]))
 else:
-    print("\033[31mYou can´t make a triangle with these lengths"
-          "\nBecause the sum between {} and {} isn´t bigger than {}\033[m".format(lin1, lin2, lin3))
+    print("{}You can´t make a triangle with these lengths"
+          "\nBecause the sum between {} and {} isn´t bigger than {}{}".format(colors["red"], lin1, lin2, lin3, colors["clean"]))
