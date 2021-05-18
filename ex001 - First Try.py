64f59d513cc572d3ff8b4599da929eaf1e957876
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-18 17:11:12 -0300

    Organization

--- ex001 - First Try.py
+++
@@ -1,9 +0,0 @@
-colors = {"clean": "\033[m",
-          "red": "\033[31m",
-          "green": "\033[32m",
-          "yellow": "\033[33m",
-          "blue": "\033[34m",
-          "purple": "\033[35m",
-          "cian": "\033[36m"}
-msg = "Hello World!!!"
-print("{}{}".format(colors["cian"], msg))
