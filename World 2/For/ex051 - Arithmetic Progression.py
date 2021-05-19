87002bd5d7ec394ef6bca54d0fb3b0651d5414ba
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-19 18:27:05 -0300

    update

--- World 2/For/ex051 - Arithmetic Progression.py
+++ World 2/For/ex051 - Arithmetic Progression.py
@@ -5,3 +5,8 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+term = int(input("Enter the first term: "))
+r = int(input("Enter the razon: "))
+for c in range(1, 11):
+    print("{}--->{}{}".format(colors["cian"], term, colors["clean"]), end=" ")
+    term += r
