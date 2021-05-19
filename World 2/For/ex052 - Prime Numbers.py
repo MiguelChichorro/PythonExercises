353365f7cb08589fc43c85401e9ca62fd3c05880
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-19 18:50:59 -0300

    update

--- World 2/For/ex052 - Prime Numbers.py
+++ World 2/For/ex052 - Prime Numbers.py
@@ -5,3 +5,19 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+num = int(input("Enter a number: "))
+cont = 0
+print("-" * 50)
+for c in range(1, num + 1):
+    if (num % c) == 0:
+        cont += 1
+        print("{}".format(colors["green"]), end=" ")
+    else:
+        print("{}".format(colors["red"]), end=" ")
+    print("{}".format(c), end=" ")
+
+print("\n{}The number {} can be divided {} times{}".format(colors["yellow"], num, cont, colors["clean"]))
+if cont == 2:
+    print("{}Your number is prime{}".format(colors["green"], colors["clean"]))
+else:
+    print("{}Your number is not prime{}".format(colors["red"], colors["clean"]))
