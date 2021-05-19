4348e8f786dbf362da804e5430d8e8d9dc50b378
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-19 19:06:59 -0300

    update

--- World 2/For/ex053 - Palindromes Detector.py
+++ World 2/For/ex053 - Palindromes Detector.py
@@ -5,3 +5,15 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ph = str(input("Enter any phrase: ")).strip().upper()
+print("{}You enter with the phrase {}{}".format(colors["cian"], ph, colors["clean"]))
+words = ph.split()
+togt = "".join(words)
+inv = ""
+for letter in range(len(togt) - 1, -1, -1):
+    inv += togt[letter]
+print("{}The reverse is {}{}".format(colors["yellow"], inv, colors["clean"]))
+if inv == togt:
+    print("{}{} is a palindrome{}".format(colors["green"], ph, colors["clean"]))
+else:
+    print("{}{} is not a palindrome{}".format(colors["red"], ph, colors["clean"]))
