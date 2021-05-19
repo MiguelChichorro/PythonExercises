34e3e9a7dad39167c621e363473864f80e6ba064
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-19 19:48:30 -0300

    update

--- World 2/For/ex055 - Sequence Bigger and Smaller.py
+++ World 2/For/ex055 - Sequence Bigger and Smaller.py
@@ -5,3 +5,16 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+h = 0
+l = 0
+for c in range(1, 6):
+    kg = float(input("Enter a weight in kg: "))
+    if c == 1:
+        h = kg
+        l = kg
+    else:
+        if kg > h:
+            h = kg
+        if kg < l:
+            l = kg
+print("{}The smaller weight is {} and the bigger is {}{}".format(colors["cian"], l, h, colors["clean"]))
