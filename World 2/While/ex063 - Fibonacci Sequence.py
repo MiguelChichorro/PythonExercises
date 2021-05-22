05c71ddc5c7d5e4e8fd8d586ecd7a16bdf393d7f
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-22 15:52:26 -0300

    Add Cod

--- World 2/While/ex063 - Fibonacci Sequence.py
+++ World 2/While/ex063 - Fibonacci Sequence.py
@@ -5,3 +5,13 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+cont = 0
+n1, n2 = 0, 1
+nterm = int(input("Enter How many terms you want: "))
+while cont < nterm:
+    print("{}{}{}".format(colors["yellow"], n1, colors["clean"]), end=" -> ")
+    nte = n1 + n2
+    n1 = n2
+    n2 = nte
+    cont += 1
+print("END", end="")
