16d07a15ff3cb0a30d5929f01a53edee26b54f1d
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-22 12:53:59 -0300

    Add Cod

--- World 2/While/ex061 - Arithmetic Progression 2.0.py
+++ World 2/While/ex061 - Arithmetic Progression 2.0.py
@@ -5,3 +5,19 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+print("=" * 22)
+print("{}10 AP TERMS USING WHILE{}".format(colors["cian"], colors["clean"]))
+print("=" * 22)
+while ans == 1:
+    cont = 0
+    term = int(input("Enter the first term: "))
+    r = int(input("Enter the razon: "))
+    while cont < 10:
+        print("{}{} {}".format(colors["yellow"], term, colors["clean"]), end="-> ")
+        term += r
+        cont += 1
+    print("End")
+    ans = int(input("{}Press [ 1 ] to do again or another number to leave: {}".format(colors["cian"], colors["clean"])))
+if ans != 1:
+    print("{}Have a good day!{}".format(colors["green"], colors["clean"]))
