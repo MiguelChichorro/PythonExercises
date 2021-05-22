e0119ad468c8a0fa3d50591d90948185a3d909de
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-22 19:29:58 -0300

    Add while loop

--- World 2/While/ex063 - Fibonacci Sequence.py
+++ World 2/While/ex063 - Fibonacci Sequence.py
@@ -7,11 +7,16 @@ colors = {"clean": "\033[m",
           "cian": "\033[36m"}
 cont = 0
 n1, n2 = 0, 1
+ans = 1
 nterm = int(input("Enter How many terms you want: "))
-while cont < nterm:
-    print("{}{}{}".format(colors["yellow"], n1, colors["clean"]), end=" -> ")
-    nte = n1 + n2
-    n1 = n2
-    n2 = nte
-    cont += 1
-print("END", end="")
+while ans == 1:
+    while cont < nterm:
+        print("{}{}{}".format(colors["yellow"], n1, colors["clean"]), end=" -> ")
+        nte = n1 + n2
+        n1 = n2
+        n2 = nte
+        cont += 1
+    print("END", end="")
+    ans = int(input("{}\nIf you want to do again\nEnter [ 1 ]\n{}Another number to leave:{} ".format(colors["blue"], colors["clean"])))
+if ans != 1:
+    print("{}Have a good day!{}".format(colors["green"], colors["clean"]))
