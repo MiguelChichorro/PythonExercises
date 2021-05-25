43eaf02d2b24199b648c2bf94b11c5cc4786f99c
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-05-24 22:20:36 -0300

    Add Code

--- World 2/Do While/ex066 - Multiple Numbers.py
+++ World 2/Do While/ex066 - Multiple Numbers.py
@@ -5,3 +5,21 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    n = cont = sum = 0
+    while True:
+        n = int(input("Enter how many numbers you want (999 stop): "))
+        if n == 999:
+            break
+        cont += 1
+        sum += n
+    if cont == 0:
+        print(f"{colors['red']}You need to enter a number{colors['clean']}")
+    elif cont == 1:
+        print(f"{colors['yellow']}You just enter the number {cont} can´t do the sum{colors['clean']}")
+    else:
+        print(f"{colors['green']}You enter {cont} numbers and the sum between them is {sum}{colors['clean']}")
+        ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
