ca00449e665b9bfa8202713239706cee38aae31d
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-11 14:14:57 -0300

    Add if error and var cont

--- World 3/Tuples/ex072 - Number Out.py
+++ World 3/Tuples/ex072 - Number Out.py
@@ -9,9 +9,13 @@ num = ("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "
        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty")
 ans = 1
 while ans == 1:
-    usunum = -1
+    usunum, cont = -1, 0
     while (usunum < 0) or (usunum > 20):
-        usunum = int(input("Enter a number between 0 and 20: "))
+        cont += 1
+        if cont == 1:
+            usunum = int(input("Enter a number between 0 and 20: "))
+        else:
+            usunum = int(input(f"{colors['red']}Enter a number between 0 and 20: {colors['clean']}"))
     print(f"{colors['blue']}You enter the number {num[usunum]}{colors['clean']}")
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
 if ans != 1:
