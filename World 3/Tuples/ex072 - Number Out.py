b904154cf42ef1d03113ce4a1a1d7ac59e4fee29
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-11 14:10:59 -0300

    Update the Code

--- World 3/Tuples/ex072 - Number Out.py
+++ World 3/Tuples/ex072 - Number Out.py
@@ -5,3 +5,14 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+num = ("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+       "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty")
+ans = 1
+while ans == 1:
+    usunum = -1
+    while (usunum < 0) or (usunum > 20):
+        usunum = int(input("Enter a number between 0 and 20: "))
+    print(f"{colors['blue']}You enter the number {num[usunum]}{colors['clean']}")
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
