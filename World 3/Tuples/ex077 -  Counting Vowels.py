2264cd29fb772b789c4208c29f5d3c66ae408010
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-12 20:02:17 -0300

    Add code

--- World 3/Tuples/ex077 -  Counting Vowels.py
+++ World 3/Tuples/ex077 -  Counting Vowels.py
@@ -5,3 +5,19 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    tuple = ()
+    ans2 = 1
+    while ans2 == 1:
+        word = str(input("Enter any word: ")).upper()
+        tuple += word,
+        ans2 = int(input(f"{colors['green']}\nPress [ 1 ] to enter another word or another number to leave: {colors['clean']}"))
+    for i in tuple:
+        print(f"\nIn the word {i} we have ", end="")
+        for letter in i:
+            if letter.lower() in "aeiou":
+                print(letter, end=" ")
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
