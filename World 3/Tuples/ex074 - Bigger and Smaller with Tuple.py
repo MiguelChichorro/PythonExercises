eb92019e3b83b0ead10f26e5625666e7d7685316
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-12 17:51:46 -0300

    Add Code

--- World 3/Tuples/ex074 - Bigger and Smaller with Tuple.py
+++ World 3/Tuples/ex074 - Bigger and Smaller with Tuple.py
@@ -1,3 +1,4 @@
+from random import randint
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -5,3 +6,15 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    tuple = ()
+    for c in range(5):
+        n = randint(1, 9)
+        tuple += n,
+    print(f"{colors['green']}The values are: {sorted(tuple)}{colors['clean']}")
+    print(f"{colors['green']}The bigger value was {max(tuple)}{colors['clean']}")
+    print(f"{colors['green']}The smaller value was {min(tuple)}{colors['clean']}")
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
