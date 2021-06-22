2ff2379642a7fa043883f52cd71a543b930fe0e2
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-22 14:57:05 -0300

    Add var sum and total price in the end

--- World 3/Tuples/ex076 - Price List with Truple.py
+++ World 3/Tuples/ex076 - Price List with Truple.py
@@ -9,21 +9,25 @@ ans = 1
 while ans == 1:
     tuple = ()
     ans2 = 1
+    sum = 0
     while ans2 == 1:
         produ = str(input("Enter the product name: "))
         tuple += produ,
         price = int(input("Enter the product price: "))
+        sum += price
         tuple += price,
         ans2 = int(input(f"{colors['green']}\nPress [ 1 ] to enter another product or another number to leave: {colors['clean']}"))
-    print(f"{colors['cian']}={colors['clean']}" * 30)
+    print(f"{colors['cian']}={colors['clean']}" * 27)
     print(f"{colors['cian']}{'Buy List': ^50}{colors['clean']}")
-    print(f"{colors['cian']}={colors['clean']}" * 30)
+    print(f"{colors['cian']}={colors['clean']}" * 27)
     for i in tuple:
         if type(i) is str:
             print(f'{i:.<32}', end='')
         else:
             print(f'US$ {i:>5.2f}')
-    print(f"{colors['cian']}={colors['clean']}" * 30)
+    print(f"{colors['cian']}={colors['clean']}" * 27)
+    print(f"{'Total Price':.<32}", end="")
+    print(f'US$ {sum:>5.2f}')
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
 if ans != 1:
     print(f"{colors['green']}Have a good day!{colors['clean']}")
