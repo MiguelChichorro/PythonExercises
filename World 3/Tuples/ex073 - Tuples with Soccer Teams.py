4992cff10fa6a2372715acbf74fbd1ac7eb0364a
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-22 14:39:42 -0300

    delete "0" in tuple

--- World 3/Tuples/ex073 - Tuples with Soccer Teams.py
+++ World 3/Tuples/ex073 - Tuples with Soccer Teams.py
@@ -5,20 +5,20 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
-teams = ("0", "Fortaleza", "Athletico-PR", "Atlético-GO", "Bragantino", "Bahia", "Fluminense", "Palmeiras", "Flamengo",
+teams = ("Fortaleza", "Athletico-PR", "Atlético-GO", "Bragantino", "Bahia", "Fluminense", "Palmeiras", "Flamengo",
          "Atlético-MG",
          "Corinthians", "Ceará", "Santos", "Cuiabá", "Sport", "São Paulo", "Juventude", "Inter", "Grêmio", "América-MG",
          "Chapecoense")
 print(f"{colors['green']}The teams are: {colors['clean']}")
-for c in sorted(teams[1:]):
+for c in sorted(teams):
     print(f"{c}")
 
 print(f"{colors['yellow']}The 5 first placed are")
-for c in teams[1:6]:
+for c in teams[0:5]:
     print(f"{c}", end=" - ")
 
 print(f"{colors['blue']}\n\nThe 4 last placed are")
 for c in teams[-4:]:
     print(f"{c}", end=" - ")
 
-print(f"\n\n{colors['red']}The Chapecoense team is in {teams.index('Chapecoense')} place{colors['clean']}")
+print(f"\n\n{colors['red']}The Chapecoense team is in {teams.index('Chapecoense')+1} place{colors['clean']}")
