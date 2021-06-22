a4168d75fab032e2346ee5f986b8bcb9996936bd
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-22 14:34:46 -0300

    Change the while to a while true

--- World 3/Tuples/ex072 - Number Out.py
+++ World 3/Tuples/ex072 - Number Out.py
@@ -10,12 +10,10 @@ num = ("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "
 ans = 1
 while ans == 1:
     usunum, cont = -1, 0
-    while (usunum < 0) or (usunum > 20):
-        cont += 1
-        if cont == 1:
-            usunum = int(input("Enter a number between 0 and 20: "))
-        else:
-            usunum = int(input(f"{colors['red']}Enter a number between 0 and 20: {colors['clean']}"))
+    while True:
+        usunum = int(input("Enter a number between 0 and 20: "))
+        if 0 <= usunum <= 20:
+            break
     print(f"{colors['blue']}You enter the number {num[usunum]}{colors['clean']}")
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
 if ans != 1:
