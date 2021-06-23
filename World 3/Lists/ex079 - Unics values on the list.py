f3fda9812e1584b7b50dfd7dabba4e24c20d4d9a
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-23 20:01:12 -0300

    Add Code

---
+++ World 3/Lists/ex079 - Unics values on the list.py
@@ -0,0 +1,26 @@
+from time import sleep
+colors = {"clean": "\033[m",
+          "red": "\033[31m",
+          "green": "\033[32m",
+          "yellow": "\033[33m",
+          "blue": "\033[34m",
+          "purple": "\033[35m",
+          "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    values = list()
+    while True:
+        print("if you want to stop just enter 0")
+        n = int(input("Enter a number:"))
+        if n not in values:
+            values.append(n)
+        if 0 in values:
+            values.remove(0)
+            values.sort()
+            break
+    print(f"{colors['blue']}Reading data...{colors['clean']}")
+    sleep(1)
+    print(f"{colors['yellow']}Your list is {values}{colors['clean']}")
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
