4686290cfd506ad600c802ff56e391df91acbfc6
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-06-23 19:41:19 -0300

    Add option to do again with while

--- World 3/Lists/ex078 - Bigger an Smaller on the list.py
+++ World 3/Lists/ex078 - Bigger an Smaller on the list.py
@@ -6,16 +6,21 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
-values = list()
-for c in range(0, 5):
-    values.append(int(input(f"Enter a number to the position {c}: ")))
-print(f"{colors['blue']}Reading data...{colors['clean']}")
-sleep(1)
-print(f"{colors['green']}The bigger value is {max(values)} and you find him in the positions", end=" ")
-for c, v in enumerate(values):
-    if v == max(values):
-        print(f"{c}...", end="")
-print(f"\n{colors['yellow']}The smaller value is {min(values)} and you find him in the positions", end=" ")
-for i, v in enumerate(values):
-    if v == min(values):
-        print(f"{i}...", end="")
+ans = 1
+while ans == 1:
+    values = list()
+    for c in range(0, 5):
+        values.append(int(input(f"Enter a number to the position {c}: ")))
+    print(f"{colors['blue']}Reading data...{colors['clean']}")
+    sleep(1)
+    print(f"{colors['green']}The bigger value is {max(values)} and you find him in the positions", end=" ")
+    for c, v in enumerate(values):
+        if v == max(values):
+            print(f"{c}...", end="")
+    print(f"\n{colors['yellow']}The smaller value is {min(values)} and you find him in the positions", end=" ")
+    for i, v in enumerate(values):
+        if v == min(values):
+            print(f"{i}...", end="")
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
