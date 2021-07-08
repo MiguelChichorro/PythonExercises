a7e46731a2f8c54e45f2c82535204c68241b0c04
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-08 18:44:34 -0300

    Change result in column

--- World 3/Lists/ex087 - Matrix 2.0.py
+++ World 3/Lists/ex087 - Matrix 2.0.py
@@ -9,7 +9,7 @@ colors = {"clean": "\033[m",
 ans = 1
 while ans == 1:
     m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    sumall = 0
+    sumall = scol = 0
     for l in range(0, 3):
         for c in range(0, 3):
             m[l][c] = int(input("Enter a number: "))
@@ -24,7 +24,9 @@ while ans == 1:
         print()
     print(f"{colors['purple']}The even sum is {sumall}{colors['clean']}")
     print(f"{colors['purple']}The bigger value in the line 2 is {max(m[1])}{colors['clean']}")
-    print(f"{colors['purple']}The all values in the line 3 sum is {sum(m[2])}{colors['clean']}")
+    for l in range(0, 3):
+        scol += m[l][2]
+    print(f"{colors['purple']}The all values in the line 3 sum is {scol}{colors['clean']}")
     print("-=-" * 30)
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
 if ans != 1:
