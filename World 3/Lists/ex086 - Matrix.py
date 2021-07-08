38438647ed839508d063977425c6e145109e90d3
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-08 16:12:13 -0300

    Exercise Finished

--- World 3/Lists/ex086 - Matrix.py
+++ World 3/Lists/ex086 - Matrix.py
@@ -6,3 +6,18 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for l in range(0, 3):
+        for c in range(0, 3):
+            m[l][c] = int(input(f"Enter a number for [{l}, {c}]: "))
+    print(f"{colors['blue']}Reading data...{colors['clean']}")
+    sleep(1)
+    for l in range(0, 3):
+        for c in range(0, 3):
+            print(f"[ {m[l][c]} ]", end=" ")
+        print()
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
