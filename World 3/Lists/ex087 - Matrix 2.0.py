5d4a19d3a4a51a407e2304fc3249ec712c797edf
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-08 16:53:27 -0300

    Exercise Finished

--- World 3/Lists/ex087 - Matrix 2.0.py
+++ World 3/Lists/ex087 - Matrix 2.0.py
@@ -6,3 +6,26 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    sumall = 0
+    for l in range(0, 3):
+        for c in range(0, 3):
+            m[l][c] = int(input("Enter a number: "))
+            if m[l][c] % 2 == 0:
+                sumall += m[l][c]
+    print(f"{colors['blue']}Reading data...{colors['clean']}")
+    sleep(1)
+    print("-=-" * 30)
+    for l in range(0, 3):
+        for c in range(0, 3):
+            print(f"{colors['blue']}[ {m[l][c]} ]{colors['clean']}", end=" ")
+        print()
+    print(f"{colors['purple']}The even sum is {sumall}{colors['clean']}")
+    print(f"{colors['purple']}The bigger value in the line 2 is {max(m[1])}{colors['clean']}")
+    print(f"{colors['purple']}The all values in the line 3 sum is {sum(m[2])}{colors['clean']}")
+    print("-=-" * 30)
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
