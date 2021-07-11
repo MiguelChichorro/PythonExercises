38d07051c735b6fe9fd07a5fe8632a14bf124059
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-11 14:08:50 -0300

    Exercise Finished

--- World 3/Dictionary/ex093 - Soccer Player Record.py
+++ World 3/Dictionary/ex093 - Soccer Player Record.py
@@ -6,3 +6,22 @@ colors = {"clean": "\033[m",
           "blue": "\033[34m",
           "purple": "\033[35m",
           "cian": "\033[36m"}
+ans = 1
+while ans == 1:
+    player = dict()
+    sumtotal = 0
+    player['name'] = str(input("Enter your name: "))
+    player['rounds'] = int(input("How many rounds do you played? "))
+    for c in range(0, player['rounds']):
+        n = int(input(f"how many gols did you do in the {c + 1}° round? "))
+        sumtotal += n
+    print(f"{colors['blue']}Reading data...{colors['clean']}")
+    sleep(1)
+    print("=" * 30)
+    print(f"{colors['yellow']}Hello {player['name']}{colors['clean']}")
+    print(f"{colors['yellow']}You played {player['rounds']} {'round' if player['rounds'] == 1 else 'rounds'} in this championship{colors['clean']}")
+    print(f"{colors['yellow']}You did {sumtotal} {'gol' if sumtotal == 1 else 'gols'} this championship{colors['clean']}")
+    print("=" * 30)
+    ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
+if ans != 1:
+    print(f"{colors['green']}Have a good day!{colors['clean']}")
