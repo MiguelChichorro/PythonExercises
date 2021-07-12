a55fb5f590bb22453c75959c3f95b4ba4a4bdc25
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-12 15:36:33 -0300

    Fixed

--- World 3/Dictionary/ex093 - Soccer Player Record.py
+++ World 3/Dictionary/ex093 - Soccer Player Record.py
@@ -12,7 +12,7 @@ while ans == 1:
     player = dict()
     sumtotal = 0
     player['name'] = str(input("Enter your name: "))
-    player['match'] = int(input("How many matches do you played? "))
+    player['match'] = int(input("How many matches did you play? "))
     for c in range(0, player['match']):
         n = int(input(f"how many goals did you do in the {c + 1}° match? "))
         sumtotal += n
@@ -23,7 +23,7 @@ while ans == 1:
     print(f"{colors['yellow']}Hello {player['name']}{colors['clean']}")
     print(f"{colors['yellow']}You played {player['match']} {'match' if player['match'] == 1 else 'matches'} in this championship{colors['clean']}")
     for c, v in enumerate(matches):
-        print(f"In the {c + 1}° match you did {v} goals")
+        print(f"{colors['yellow']}In the {c + 1}° match you did {v} goals{colors['clean']}")
     print(f"{colors['yellow']}You did {sumtotal} {'goal' if sumtotal == 1 else 'goals'} this championship{colors['clean']}")
     print("=" * 30)
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
