b64b50a8c402d1fc1f95f1ed876f92df9edb94fe
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-14 13:43:24 -0300

    Exercise Finished

---
+++ World 3/Functions/ex097 - Special Print.py
@@ -0,0 +1,17 @@
+colors = {"clean": "\033[m",
+          "red": "\033[31m",
+          "green": "\033[32m",
+          "yellow": "\033[33m",
+          "blue": "\033[34m",
+          "purple": "\033[35m",
+          "cian": "\033[36m"}
+
+
+def write(txt):
+    tam = len(txt)
+    print("=" * tam)
+    print(f"{colors['blue']}{txt}{colors['clean']}")
+    print("=" * tam)
+
+
+write(txt=str(input("Write anything: ")))
