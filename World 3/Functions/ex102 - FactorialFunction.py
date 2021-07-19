5eb772eca4eb720b5e5ca403f9429bfb1e5b21bf
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-19 18:44:17 -0300

    add function choice()

--- World 3/Functions/ex102 - FactorialFunction.py
+++ World 3/Functions/ex102 - FactorialFunction.py
@@ -1,4 +1,5 @@
 from time import sleep
+from utilities import database
 colors = {"clean": "\033[m",
           "red": "\033[31m",
           "green": "\033[32m",
@@ -33,9 +34,8 @@ def factorial(a=0, b="", show=False):
 ans = 1
 while ans == 1:
     n = int(input("Enter the number you want to do a factorial: "))
-    ans2 = str(input(f"Do you want do add another person? [{colors['green']}Y{colors['clean']}/{colors['red']}N{colors['clean']}]: ")).strip().upper()[0]
-    while ans2 != "Y" and ans2 != "N":
-        ans2 = str(input(f"{colors['red']}Please just enter Yes or No {colors['clean']}[{colors['green']}Y{colors['clean']}/{colors['red']}N{colors['clean']}]: ")).strip().upper()[0]
+    ans2 = str(input(f"Do you want to see the calule? [{colors['green']}Y{colors['clean']}/{colors['red']}N{colors['clean']}]: ")).strip().upper()[0]
+    database.Choice(ans2)
     factorial(n, ans2)
     ans = int(input(f"{colors['cian']}\nPress [ 1 ] to do again or another number to leave: {colors['clean']}"))
 if ans != 1:
