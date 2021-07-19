4c3894afe449a76e7f25af6b4486b7dd8815198e
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-19 17:44:11 -0300

    Exercise Finished

---
+++ World 3/Modules and Packages/ex110 - Simplifying Programs.py
@@ -0,0 +1,13 @@
+import ultilities
+from time import sleep
+colors = {"clean": "\033[m",
+          "red": "\033[31m",
+          "green": "\033[32m",
+          "yellow": "\033[33m",
+          "blue": "\033[34m",
+          "purple": "\033[35m",
+          "cian": "\033[36m"}
+n = int(input("Enter How much money do you want to converse: R$"))
+print(f"{colors['purple']}Reading data...{colors['clean']}")
+sleep(1)
+ultilities.resum(n, 19, 28, True)
